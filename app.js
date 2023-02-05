1fe1d2eeac1049604d20ebc63b4cd9d4a8d645b2
Ali Gezik <82276433+mealigezik@users.noreply.github.com>
2023-02-05 02:04:32 +0100

    new

---
+++ app.js
@@ -0,0 +1,40 @@
+const symbols = "!@#$%^&*()\\_+~|}{[]:;?><,./-=";
+const numbers = "0123456789";
+const upperLetters = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const lowerLetters = "abcdefghijklmnopqrstuvwxyz";
+let sum = "";
+let sum1 = "";
+
+const button = document.querySelector(".btn");
+const input = document.getElementById("pw");
+
+button.addEventListener("click", () => {
+  randomPW();
+  input.value = "";
+  input.value = sum1;
+  //sum1 = "";
+  sum = "";
+  // input.value = "";
+});
+
+function randomPW() {
+  for (let i = 0; i <= 2; i++) {
+    sum += symbols.charAt(Math.floor(Math.random() * symbols.length));
+  }
+  for (let i = 1; i <= 3; i++) {
+    sum += numbers.charAt(Math.floor(Math.random() * numbers.length));
+  }
+  for (let i = 1; i <= 3; i++) {
+    sum += upperLetters.charAt(Math.floor(Math.random() * upperLetters.length));
+  }
+  for (let i = 1; i <= 2; i++) {
+    sum += lowerLetters.charAt(Math.floor(Math.random() * lowerLetters.length));
+  }
+
+  sum1 = [...sum];
+  sum1 = sum1.sort(() => Math.random() - 0.5).join("");
+
+  // return sum1;
+  // console.log(sum); //?shufflesiz
+  // console.log(sum1); //!shuffleli
+}
